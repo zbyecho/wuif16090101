fe13baea46f0a46e8a47bf3437f5eaa7a97235a6
zbyecho <1726164169@qq.com>
2017-03-09 18:54:59 +0800

    gh-ceshi
    index

--- waw/zbyjs/zby_index.js
+++ waw/zbyjs/zby_index.js
@@ -1,32 +1,97 @@
-var qubtn=document.querySelector('.zby_quxiao');
-var rest=document.querySelector('.zby_rest');
-var secrh=document.querySelector('.zby_st_box');
-var shou=document.querySelector('.header-shousuo ');
-console.log(qubtn);
-console.log(rest);
-console.log(secrh);
-console.log(shou);
-rest.addEventListener('touchstart',function () {
-    secrh.style.opacity=0;
-    secrh.style.transition='all .5s';
-    secrh.style.zIndex=-1;
-});
-shou.addEventListener('touchstart',function () {
-    secrh.style.opacity=1;
-    secrh.style.transition='all .5s';
-    secrh.style.zIndex=9999;
-});
-qubtn.addEventListener('touchstart',function () {
-  secrh.style.opacity=0;
-    secrh.style.transition='all .5s';
-    secrh.style.zIndex=-1;
-});
-he();
-window.onresize=he;
-function he() {
-    var winh=document.documentElement.clientHeight;
-    var winw=document.documentElement.clientWidth;
-    console.log(winh)
-    // secrh.style.height=winh+'px';
-    // secrh.style.width=winw+'px';
+// 导航开始
+    var qubtn=document.querySelector('.zby_quxiao');
+    var rest=document.querySelector('.zby_rest');
+    var secrh=document.querySelector('.zby_st_box');
+    var shou=document.querySelector('.header-shousuo ');
+    console.log(qubtn);
+    console.log(rest);
+    console.log(secrh);
+    console.log(shou);
+    rest.addEventListener('touchstart',function () {
+        secrh.style.opacity=0;
+        secrh.style.transition='all .5s';
+        secrh.style.zIndex=-1;
+    });
+    shou.addEventListener('touchstart',function () {
+        secrh.style.opacity=1;
+        secrh.style.transition='all .5s';
+        secrh.style.zIndex=9999;
+    });
+    qubtn.addEventListener('touchstart',function () {
+      secrh.style.opacity=0;
+        secrh.style.transition='all .5s';
+        secrh.style.zIndex=-1;
+    });
+    he();
+    window.onresize=he;
+    function he() {
+        var winh=document.documentElement.clientHeight;
+        var winw=document.documentElement.clientWidth;
+        console.log(winh)
+        // secrh.style.height=winh+'px';
+        // secrh.style.width=winw+'px';
+    }
+// 导航结束
+//底部选项卡开始
+    var Btn=document.querySelectorAll('.BtnBox .BtnItems');
+    console.log(Btn)
+    var Dbox=document.querySelectorAll('.zby_Dbox');
+    console.log(Dbox);
+    var Title=document.querySelector('.header-fenlei a');
+     var flag=true
+    Dbox[0].style.display="block";
+    // cc(Btn[0])
+    var a=Btn[0].getElementsByTagName('a')
+   console.log(Btn[0].innerText) 
+
+    Title.innerHTML=Btn[0].innerHTML
+    for(var i=0;i<Btn.length;i++){
+        Btn[i].index=i;
+        Btn[i].addEventListener('touchstart',function(){
+            for(var j=0;j<Btn.length;j++){
+                Btn[j].classList.remove('zby-hot')
+                Dbox[j].style.display="none";
+            }
+            this.classList.add('zby-hot')
+            Dbox[this.index].style.display="block";
+            Title.innerHTML=this.innerHTML
+            cc(this.index);
+        })
+        
+    }
+
+   
+    function cc(i){
+        
+        if(i==0){
+                
+                console.log(i)
+                  aa()
+                  return
+        }else if(i==2){
+                console.log(i)
+                bb()
+                  return
+
+        }
+        
+    }
+var swiper= new Swiper('.swiper-container',{
+        pagination: '.swiper-pagination',
+        paginationClickable: true
+    });
+//底部选项卡结束
+function aa(){
+    var swiper= new Swiper('.swiper-container',{
+        pagination: '.swiper-pagination',
+        paginationClickable: true
+    });
 }
+function bb(){
+    var swiper2 = new Swiper('.swiper-container2',{
+            pagination: '.swiper-pagination2',
+            paginationClickable: true
+    });
+}
+     
+        
